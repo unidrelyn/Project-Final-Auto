28aadd6d84fbfc87108928c87584297638ade18d
mohamed1410 <m.hossam3330@yahoo.com>
2024-04-10 18:55:41 +0200

    signout is done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,71 +1,48 @@
+
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useContext } from 'react'; // Import useContext
+import { AuthContext } from '../context/AuthContext'; // Import AuthContext
 
-
-const NavBar = ({ isAuthenticated }) => {
-
+const NavBar = () => {
   const navigate = useNavigate();
-
-  const handleSignOut = () => {
-    setIsAuthenticated(false);
-    navigate("/login");
+  const { handleLogout, isLoggedIn } = useContext(AuthContext); // Use AuthContext
+  const signOut = async () => {
+    await handleLogout(); // Call your logout function from AuthContext
+    navigate('/'); // Redirect to home page after logout
   };
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">
-          <img
-            src={logo}
-            alt="AutoExchange Logo"
-            className="home-page-logo"
-            style={{ width: "150px" }}
-          />
+          <img src={logo} alt="AutoExchange Logo" className="home-page-logo" style={{ width: "150px" }} />
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/listings">
-                Listings
-              </NavLink>
+              <NavLink className="nav-link" to="/listings">Listings</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
-                About
-              </NavLink>
+              <NavLink className="nav-link" to="/about">About</NavLink>
             </li>
 
-            {isAuthenticated ? (
-              <>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="#" onClick={handleSignOut}>
-                    Signout
-                  </NavLink>
-                </li>
-              </>
+            {isLoggedIn ? (
+              // If logged in, show Signout option
+              <li className="nav-item">
+  <button className="nav-link btn btn-link" onClick={signOut} style={{ color: 'rgba(255,255,255,.55)' }}>Signout</button>
+</li>
             ) : (
+              // If not logged in, show Login and Signup options
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/login">
-                    Login
-                  </NavLink>
+                  <NavLink className="nav-link" to="/login">Login</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/signup">
-                    Signup
-                  </NavLink>
+                  <NavLink className="nav-link" to="/signup">Signup</NavLink>
                 </li>
               </>
             )}
@@ -77,6 +54,3 @@ const NavBar = ({ isAuthenticated }) => {
 };
 
 export default NavBar;
-
-
-
