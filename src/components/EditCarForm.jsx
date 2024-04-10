b59d4cf8b50cb6f21007d91d8da74b283b2bd044
unknown <unidrelyn@gmail.com>
2024-04-11 00:20:29 +0200

    fixed add car and API

--- src/components/EditCarForm.jsx
+++ src/components/EditCarForm.jsx
@@ -1,140 +1,139 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { API_URL } from "../config";
 
 function EditCarForm() {
-  const [carData, setCarData] = useState({
-    make: "",
-    model: "",
-    year: 0,
-    price: 0,
-    description: "",
-  });
-  const [submitting, setSubmitting] = useState(false);
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const navigate = useNavigate();
-  const { carId } = useParams();
+	const [carData, setCarData] = useState({
+		brand: "",
+		model: "",
+		year: 0,
+		price: 0,
+		description: "",
+	});
+	const [submitting, setSubmitting] = useState(false);
+	const [error, setError] = useState("");
+	const [success, setSuccess] = useState("");
+	const navigate = useNavigate();
+	const { carId } = useParams();
 
-  useEffect(() => {
-    const fetchCarDetails = async () => {
-      try {
-        if (carId) {
-          const response = await axios.get(
-            `http://localhost:3000/cars/${carId}`
-          );
-          setCarData(response.data);
-        }
-      } catch (error) {
-        console.error("Error fetching car details:", error);
-        setError("Failed to fetch car details");
-      }
-    };
+	useEffect(() => {
+		const fetchCarDetails = async () => {
+			try {
+				if (carId) {
+					const response = await axios.get(`${API_URL}/api/cars/${carId}`);
+					setCarData(response.data);
+				}
+			} catch (error) {
+				console.error("Error fetching car details:", error);
+				setError("Failed to fetch car details");
+			}
+		};
 
-    fetchCarDetails();
-  }, [carId]);
+		fetchCarDetails();
+	}, [carId]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCarData((prevData) => ({ ...prevData, [name]: value }));
-  };
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setCarData((prevData) => ({ ...prevData, [name]: value }));
+	};
 
-  const editCar = async (updatedCar) => {
-    try {
-      await axios.put(`http://localhost:3000/cars/${carId}`, updatedCar);
-      setSuccess("Car details updated successfully!");
-      setTimeout(() => navigate("/listings"), 2000);
-    } catch (error) {
-      console.error("Error updating the car:", error);
-      setError("Failed to update the car. Please try again.");
-    } finally {
-      setSubmitting(false);
-    }
-  };
+	const editCar = async (updatedCar) => {
+		try {
+			await axios.put(`${API_URL}/api/cars/${carId}`, updatedCar);
+			setSuccess("Car details updated successfully!");
+			setTimeout(() => navigate("/listings"), 2000);
+		} catch (error) {
+			console.error("Error updating the car:", error);
+			setError("Failed to update the car. Please try again.");
+		} finally {
+			setSubmitting(false);
+		}
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setSubmitting(true);
 
-    await editCar(carData);
-  };
+		await editCar(carData);
+	};
 
-  return (
-    <div>
-      <form className="d-flex flex-column mt-3 vh-100" onSubmit={handleSubmit}>
-        <div className="col-md-6 mx-auto">
-          <h2>Edit Car Info</h2>
-          <div className="form">
-            <input
-              name="make"
-              value={carData.make}
-              onChange={handleChange}
-              placeholder="Make"
-              className="form m-3"
-              style={{ width: "400px" }}
-            />
-            <input
-              name="model"
-              value={carData.model}
-              onChange={handleChange}
-              placeholder="Model"
-              className="form m-3"
-              style={{ width: "400px" }}
-            />
-            <input
-              name="year"
-              type="number"
-              value={carData.year}
-              onChange={handleChange}
-              placeholder="Year"
-              className=" form m-3"
-              style={{ width: "400px" }}
-            />
-            <input
-              name="price"
-              type="number"
-              value={carData.price}
-              onChange={handleChange}
-              placeholder="Price"
-              className="form m-3"
-              style={{ width: "400px" }}
-            />
-            <input
-              name="description"
-              value={carData.description}
-              onChange={handleChange}
-              placeholder="Description"
-              className="form m-3"
-              style={{ width: "400px" }}
-            />
-          </div>
-        </div>
-        <div className="d-flex justify-content-center">
-          <button
-            type="submit"
-            className="btn btn-ae-primary mt-5 mb-5"
-            style={{ width: "150px" }}
-            disabled={submitting}
-          >
-            {submitting ? "Updating..." : "Update Car"}
-          </button>
-        </div>
-        {error && <div style={{ color: "red" }}>{error}</div>}
-        {success && <div style={{ color: "green" }}>{success}</div>}
-      </form>
-      {/* Dark/Light Mode Switch */}
-      <div className="form-check form-switch position-fixed bottom-0 end-0 m-4">
-        <input
-          className="form-check-input p-2"
-          type="checkbox"
-          role="switch"
-          id="flexSwitchCheckChecked"
-          defaultChecked
-          onClick={myFunction}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<form className="d-flex flex-column mt-3 vh-100" onSubmit={handleSubmit}>
+				<div className="col-md-6 mx-auto">
+					<h2>Edit Car Info</h2>
+					<div className="form">
+						<input
+							name="brand"
+							value={carData.brand}
+							onChange={handleChange}
+							placeholder="Brand"
+							className="form m-3"
+							style={{ width: "400px" }}
+						/>
+						<input
+							name="model"
+							value={carData.model}
+							onChange={handleChange}
+							placeholder="Model"
+							className="form m-3"
+							style={{ width: "400px" }}
+						/>
+						<input
+							name="year"
+							type="number"
+							value={carData.year}
+							onChange={handleChange}
+							placeholder="Year"
+							className=" form m-3"
+							style={{ width: "400px" }}
+						/>
+						<input
+							name="price"
+							type="number"
+							value={carData.price}
+							onChange={handleChange}
+							placeholder="Price"
+							className="form m-3"
+							style={{ width: "400px" }}
+						/>
+						<input
+							name="description"
+							value={carData.description}
+							onChange={handleChange}
+							placeholder="Description"
+							className="form m-3"
+							style={{ width: "400px" }}
+						/>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center">
+					<button
+						type="submit"
+						className="btn btn-ae-primary mt-5 mb-5"
+						style={{ width: "150px" }}
+						disabled={submitting}
+					>
+						{submitting ? "Updating..." : "Update Car"}
+					</button>
+				</div>
+				{error && <div style={{ color: "red" }}>{error}</div>}
+				{success && <div style={{ color: "green" }}>{success}</div>}
+			</form>
+			{/* Dark/Light Mode Switch */}
+			<div className="form-check form-switch position-fixed bottom-0 end-0 m-4">
+				<input
+					className="form-check-input p-2"
+					type="checkbox"
+					role="switch"
+					id="flexSwitchCheckChecked"
+					defaultChecked
+					onClick={myFunction}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default EditCarForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,164 +1,165 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCarList } from "../context/CarListContext";
 import axios from "axios";
 import HeroWide from "../assets/HeroWide.jpg";
 import { useCart } from "../context/CartContext"; // Import useCart hook
+import { API_URL } from "../config";
 
 const HomePage = () => {
-  const [carListings, setCarListings] = useState([]);
-  const { carList } = useCarList();
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigate = useNavigate();
-  const { addToCart } = useCart(); // Use addToCart function from useCart hook
+	const [carListings, setCarListings] = useState([]);
+	const { carList } = useCarList();
+	const [searchTerm, setSearchTerm] = useState("");
+	const navigate = useNavigate();
+	const { addToCart } = useCart(); // Use addToCart function from useCart hook
 
-  useEffect(() => {
-    fetchCarListings();
-  }, []);
+	useEffect(() => {
+		fetchCarListings();
+	}, []);
 
-  const fetchCarListings = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/cars");
-      setCarListings(response.data);
-    } catch (error) {
-      console.error("Error fetching car listings:", error);
-    }
-  };
+	const fetchCarListings = async () => {
+		try {
+			const response = await axios.get(`${API_URL}/api/cars`);
+			setCarListings(response.data);
+		} catch (error) {
+			console.error("Error fetching car listings:", error);
+		}
+	};
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value.toLowerCase());
-  };
+	const handleSearchChange = (event) => {
+		setSearchTerm(event.target.value.toLowerCase());
+	};
 
-  const handleEdit = (carId) => {
-    navigate(`/edit/${carId}`);
-  };
+	const handleEdit = (carId) => {
+		navigate(`/edit/${carId}`);
+	};
 
-  const handleDelete = async (carId) => {
-    if (window.confirm("Are you sure you want to delete this car?")) {
-      try {
-        await axios.delete(`http://localhost:3000/cars/${carId}`);
-        setCarListings((prevListings) =>
-          prevListings.filter((car) => car.id !== carId)
-        );
-      } catch (error) {
-        console.error("Error deleting car:", error);
-      }
-    }
-  };
+	const handleDelete = async (carId) => {
+		if (window.confirm("Are you sure you want to delete this car?")) {
+			try {
+				await axios.delete(`${API_URL}/api/cars/${carId}`);
+				setCarListings((prevListings) =>
+					prevListings.filter((car) => car.id !== carId)
+				);
+			} catch (error) {
+				console.error("Error deleting car:", error);
+			}
+		}
+	};
 
-  const handleAddToCart = (carId) => {
-    const carToAdd = carListings.find((car) => car.id === carId);
-    if (carToAdd) {
-      addToCart(carToAdd); // Add car to cart
-      navigate("/cart"); // Navigate to cart page
-    }
-  };
+	const handleAddToCart = (carId) => {
+		const carToAdd = carListings.find((car) => car.id === carId);
+		if (carToAdd) {
+			addToCart(carToAdd); // Add car to cart
+			navigate("/cart"); // Navigate to cart page
+		}
+	};
 
-  const handleAddCar = () => {
-    navigate("/add-car");
-  };
+	const handleAddCar = () => {
+		navigate("/add-car");
+	};
 
-  const filteredListings = carListings.filter(
-    (car) =>
-      searchTerm === "" ||
-      (car.make && car.make.toLowerCase().includes(searchTerm)) ||
-      (car.model && car.model.toLowerCase().includes(searchTerm))
-  );
+	const filteredListings = carListings.filter(
+		(car) =>
+			searchTerm === "" ||
+			(car.make && car.make.toLowerCase().includes(searchTerm)) ||
+			(car.model && car.model.toLowerCase().includes(searchTerm))
+	);
 
-  const capitalizeFirstLetter = (str) => {
-    return str.replace(/\b\w/g, (char) => char.toUpperCase());
-  };
+	const capitalizeFirstLetter = (str) => {
+		return str.replace(/\b\w/g, (char) => char.toUpperCase());
+	};
 
-  return (
-    <div className="home-page-container">
-      <div className="hero-container position-relative">
-        <img src={HeroWide} alt="Car Image" className="hero-image img-fluid" />
-        <div
-          className="overlay-content position-absolute top-0 start-50 translate-middle text-center"
-          style={{
-            paddingTop: "200px",
-            "@media (minWidth: 576px)": { paddingTop: "200px" },
-          }}
-        >
-          <h1 className="main-heading mt-5 mb-4" style={{ color: "white" }}>
-            Welcome to AutoExchange
-          </h1>
-          <p className="description" style={{ color: "white" }}>
-            Discover your ideal car today or list your vehicle for sale with
-            ease.
-          </p>
-        </div>
-      </div>
+	return (
+		<div className="home-page-container">
+			<div className="hero-container position-relative">
+				<img src={HeroWide} alt="Car Image" className="hero-image img-fluid" />
+				<div
+					className="overlay-content position-absolute top-0 start-50 translate-middle text-center"
+					style={{
+						paddingTop: "200px",
+						"@media (minWidth: 576px)": { paddingTop: "200px" },
+					}}
+				>
+					<h1 className="main-heading mt-5 mb-4" style={{ color: "white" }}>
+						Welcome to AutoExchange
+					</h1>
+					<p className="description" style={{ color: "white" }}>
+						Discover your ideal car today or list your vehicle for sale with
+						ease.
+					</p>
+				</div>
+			</div>
 
-      <div className="listings-page-container">
-        <div
-          className="d-flex justify-content-center align-items-center p-5 m-2"
-          style={{ gap: "20px", zIndex: "2" }} // Set a higher z-index for the search bar container
-        >
-          <h1 className="text">Recommended for you</h1>
-        </div>
-        {/* Dark/Light Mode Switch */}
-        <div
-          className="form-check form-switch position-fixed bottom-0 end-0 m-4"
-          style={{ zIndex: 999 }}
-        >
-          <input
-            className="form-check-input p-2"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckChecked"
-            defaultChecked
-            onClick={myFunction}
-          />
-        </div>
-        <div className="row w-100 d-flex justify-content-start">
-          {filteredListings.length > 0 ? (
-            filteredListings.map((car) => (
-              <div key={car.id} className="col d-flex justify-content-start">
-                <div
-                  className="card m-2 p-3 d-flex justify-content-center"
-                  style={{ width: "18rem" }}
-                >
-                  <img
-                    src={car.image}
-                    alt={`${car.make} ${car.model}`}
-                    className="card-img-top mx-auto" // Center the image horizontally
-                    style={{
-                      width: "100%",
-                      height: "150px",
-                      objectFit: "cover",
-                      borderRadius: "4px",
-                    }}
-                  />
-                  <h5>
-                    {car.brand && capitalizeFirstLetter(car.brand)}{" "}
-                    {car.model && capitalizeFirstLetter(car.model)}
-                  </h5>
-                  <div className="col text-left">
-                    <p>Year: {car.year}</p>
-                    <p>Price: {car.price}</p>
-                    <p>
-                      Color: {car.color && capitalizeFirstLetter(car.color)}
-                    </p>
-                  </div>
-                  <div className="col">
-                    <button
-                      className="m-2 btn btn-ae-primary"
-                      onClick={() => handleAddToCart(car.id)}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No car listings available.</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+			<div className="listings-page-container">
+				<div
+					className="d-flex justify-content-center align-items-center p-5 m-2"
+					style={{ gap: "20px", zIndex: "2" }} // Set a higher z-index for the search bar container
+				>
+					<h1 className="text">Recommended for you</h1>
+				</div>
+				{/* Dark/Light Mode Switch */}
+				<div
+					className="form-check form-switch position-fixed bottom-0 end-0 m-4"
+					style={{ zIndex: 999 }}
+				>
+					<input
+						className="form-check-input p-2"
+						type="checkbox"
+						role="switch"
+						id="flexSwitchCheckChecked"
+						defaultChecked
+						onClick={myFunction}
+					/>
+				</div>
+				<div className="row w-100 d-flex justify-content-start">
+					{filteredListings.length > 0 ? (
+						filteredListings.map((car) => (
+							<div key={car.id} className="col d-flex justify-content-start">
+								<div
+									className="card m-2 p-3 d-flex justify-content-center"
+									style={{ width: "18rem" }}
+								>
+									<img
+										src={car.image}
+										alt={`${car.make} ${car.model}`}
+										className="card-img-top mx-auto" // Center the image horizontally
+										style={{
+											width: "100%",
+											height: "150px",
+											objectFit: "cover",
+											borderRadius: "4px",
+										}}
+									/>
+									<h5>
+										{car.brand && capitalizeFirstLetter(car.brand)}{" "}
+										{car.model && capitalizeFirstLetter(car.model)}
+									</h5>
+									<div className="col text-left">
+										<p>Year: {car.year}</p>
+										<p>Price: {car.price}</p>
+										<p>
+											Color: {car.color && capitalizeFirstLetter(car.color)}
+										</p>
+									</div>
+									<div className="col">
+										<button
+											className="m-2 btn btn-ae-primary"
+											onClick={() => handleAddToCart(car.id)}
+										>
+											Add to Cart
+										</button>
+									</div>
+								</div>
+							</div>
+						))
+					) : (
+						<p>No car listings available.</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
